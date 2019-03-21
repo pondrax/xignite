8660c2b4316e7eed2e5d01f678c5e8d4bc1e73dd
drax <pondras.setiawan@gmail.com>
2019-03-21 16:44:34 +0700

    upload file

--- modules/xweb/views/home.blade.php
+++ modules/xweb/views/home.blade.php
@@ -7,7 +7,7 @@
           @if(logged())
             <span class="text-muted">Selamat datang, </span>
             <span class="text-info"><b>{{logged()->email}}</b></span><br>
-            <div class="alert alert-danger py-0 px-2">Email belum diaktivasi. Harap aktifkan email<br> anda terlebih dahulu dihalaman profil</div>
+            <div class="alert alert-danger py-0 px-2 mb-0">Email belum diaktivasi. Harap aktifkan<br> email anda sekarang dihalaman profil.<br> Email aktif lebih diprioritaskan sistem.</div>
             <a class="" href="@url/aduanmu">Aduan anda</a>
             &nbsp;
             <a class="" href="@url/profil">Profil</a>
