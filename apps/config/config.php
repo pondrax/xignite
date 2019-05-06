30c02cf8add5a45f1e2b5dd8117b57062b4b872d
drax <pondras.setiawan@gmail.com>
2019-05-06 15:00:51 +0700

    Update My Model and helper Core

--- apps/config/config.php
+++ apps/config/config.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// date_default_timezone_set('Asia/Jakarta');
 // setlocale(LC_ALL, 'IND');
 
 /*
@@ -522,7 +521,7 @@ $config['compress_output'] = FALSE;
 | helper' page of the user guide for information regarding date handling.
 |
 */
-$config['time_reference'] = 'local';
+$config['time_reference'] = APP_TIMEZONE;
 
 /*
 |--------------------------------------------------------------------------
